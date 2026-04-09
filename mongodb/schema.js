a7079c4572f8b65617f37a84f8b26b8f8a287695
Chaoscookie2 <f.bamberger@htblaleonding.onmicrosoft.com>
2026-04-09 17:45:41 +0200

    add schema creation

---
+++ mongodb/schema.js
@@ -0,0 +1,145 @@
+use blog;
+db.User.drop();
+db.Entry.drop();
+db.Category.drop();
+db.Comment.drop();
+
+db.createCollection("User", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            title: "User Object Validation",
+            additionalProperties: false,
+            required: ["_id", "displayName", "password", "birthday"],
+            properties: {
+                _id: {
+                    bsonType: "string",
+                    description: "'_id' must be a string and is required"
+                },
+                password: {
+                    bsonType: "string",
+                    description: "'password' must be a string and is required"
+                },
+                displayName: {
+                    bsonType: "string",
+                    description: "'displayName' must be a string and is required"
+                },
+                birthday: {
+                    bsonType: "date",
+                    description: "'birthday' must be a date and is required"
+                },
+                country: {
+                    bsonType: "string",
+                    description: "'country' must be a string"
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("Entry", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            title: "Entry Object Validation",
+            additionalProperties: false,
+            required: ["title", "description", "creationDate", "editDates", "impressionCount", "content", "commentsAllowed"],
+            properties: {
+                title: {
+                    bsonType: "string",
+                    description: "'title' must be a string and is required"
+                },
+                description: {
+                    bsonType: "string",
+                    description: "'description' must be a string and is required"
+                },
+                creationDate: {
+                    bsonType: "date",
+                    description: "'creationDate' must be a date and is required"
+                },
+                editDates: {
+                    bsonType: "array",
+                    items: {
+                        bsonType: "date"
+                    },
+                    description: "'editDates' must be an array of dates and is required"
+                },
+                impressionCount: {
+                    bsonType: "long",
+                    description: "'impressionCount' must be a number and is required"
+                },
+                commentsAllowed: {
+                    bsonType: "bool",
+                    description: "'commentsAllowed' must be a bool and is required"
+                },
+                content: {
+                    bsonType: "object",
+                    additionalProperties: false,
+                    required: ["text"],
+                    properties: {
+                        text: {
+                            bsonType: "string",
+                            description: "'text' must be a string and is required"
+                        },
+                        links: {
+                            bsonType: "array",
+                            items: {
+                                bsonType: "string"
+                            },
+                            description: "'links' must be an array of strings"
+                        },
+                        images: {
+                            bsonType: "array",
+                            items: {
+                                bsonType: "string"
+                            },
+                            description: "'images' must be an array of base64 encoded images"
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("Category", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            title: "Category Object Validation",
+            additionalProperties: false,
+            required: ["category"],
+            properties: {
+                category: {
+                    bsonType: "string",
+                    description: "'category' must be a string and is required"
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("Comment", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            title: "Comment Object Validation",
+            additionalProperties: false,
+            required: ["text", "likes", "creationDate"],
+            properties: {
+                text: {
+                    bsonType: "string",
+                    description: "'text' must be a string and is required"
+                },
+                likes: {
+                    bsonType: "long",
+                    description: "'likes' must be a long and is required"
+                },
+                creationDate: {
+                    bsonType: "date",
+                    description: "'creationDate' must be a date and is required"
+                }
+            }
+        }
+    }
+});
